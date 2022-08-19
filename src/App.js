e423770b31b20a5b1a66978b682572cbd2b49175
Stefan Ihle <stefanihle@gmx.net>
2022-08-19 21:01:55 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+
+import { Navbar } from "./app/Navbar";
+import PostsList from "./components/posts/PostList";
+import AddPostForm from "./components/postForm/AddPostForm";
+import SinglePostPage from "./components/posts/SinglePostPage";
+import EditPostForm from "./components/postForm/EditPostForm";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <React.Fragment>
+                <AddPostForm />
+                <PostsList />
+              </React.Fragment>
+            )}
+          />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
